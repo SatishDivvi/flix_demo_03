5cfab93116bab2a7070f8d6622137007d419625f
CSC Student <cscstudent@dhillss-MacBook-Pro-2.local>
2018-08-03 18:09:46 -0700

    created Movie Cell

---
+++ flix_demo_03/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  flix_demo_03
+//
+//  Created by CSC Student on 8/3/18.
+//  Copyright © 2018 CSC Student. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
