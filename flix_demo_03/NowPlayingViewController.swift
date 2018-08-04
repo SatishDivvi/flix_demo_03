890fc3994a63dd4e50b0bce50a14f3183402b520
CSC Student <cscstudent@dhillss-MacBook-Pro-2.local>
2018-08-03 17:14:47 -0700

    updated files

--- flix_demo_03/NowPlayingViewController.swift
+++ flix_demo_03/NowPlayingViewController.swift
@@ -41,7 +41,7 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 11
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
