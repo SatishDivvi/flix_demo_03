170be430324b296769b54049ba23a0cd7a59014e
CSC Student <cscstudent@dhillss-MacBook-Pro-2.local>
2018-08-10 21:47:57 -0700

    added posterImageView to the posterCell, added fetchMovies and overrided few methods of collectionsource in SuperHeroView Controller

--- flix_demo_03/ViewControllers/SuperHeroViewController.swift
+++ flix_demo_03/ViewControllers/SuperHeroViewController.swift
@@ -13,37 +13,70 @@ class SuperHeroViewController: UIViewController, UICollectionViewDataSource {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    
+    var movies: [[String: Any]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        collectionView.dataSource = self as! UICollectionViewDataSource
         
+        collectionView.dataSource = self
+        fetchMovies()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return movies.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCell
+        let movie = movies[indexPath.item]
+        if let posterPathString = movie["poster_path"] as? String {
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            let posterURL = URL(string: baseURLString + posterPathString)!
+            cell.posterImageView.af_setImage(withURL: posterURL)
+            
+        }
+        
+        return cell
     }
-
+    
+    func fetchMovies() {
+        //indicator.startAnimating()
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        // When a task is created it is in suspended state
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.collectionView.reloadData()
+                //self.refreshControl.endRefreshing()
+                //self.indicator.stopAnimating()
+            }
+        }
+        // resume the task
+        task.resume()
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
